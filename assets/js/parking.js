939c92e156962eedfbfa79650edf80897f3eef13
宜志富紹太 <shotagishitomi@gmail.com>
2019-11-18 02:10:52 +0900

    parking.htmlの追加 detailのボタン少し変えました

---
+++ assets/js/parking.js
@@ -0,0 +1,45 @@
+$(function(){
+    $('.map').css("opacity","0");
+    $(window).scroll(function (){
+      $(".map").each(function(){
+        var imgPos = $(this).offset().top;    
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + windowHeight/3){
+          $(this).css("opacity","1" );
+          $(this).css({ 
+            "font-size": "100px",
+            "padding": "0 20px 40px"
+          });
+        } else {
+          $(this).css("opacity","0" );
+          $(this).css({ 
+            "font-size": "20px",
+            "padding": "20px"
+          });
+        }
+      });
+    });
+
+    $('.point').css("opacity","0");
+    $(window).scroll(function (){
+      $(".point").each(function(){
+        var imgPos = $(this).offset().top;    
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > imgPos - windowHeight + windowHeight/2){
+          $(this).css("opacity","1" );
+          $(this).css({ 
+            "font-size": "100px",
+            "padding": "0 20px 40px"
+          });
+        } else {
+          $(this).css("opacity","0" );
+          $(this).css({ 
+            "font-size": "20px",
+            "padding": "20px"
+          });
+        }
+      });
+    });
+  });
